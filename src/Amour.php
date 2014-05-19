2086fc492bbb29f2da84b4abb9e8a1af74e34234
Jan Jablunka <jablunka@gmail.com>
2014-05-19 18:14:24 +0200

    incremental

---
+++ src/Amour.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Amour;
+
+class Amour {
+
+	static function success($res = NULL) {
+		$app = App::get();
+
+		$app->response->setStatus(200);
+
+		return \Katu\Utils\JSON::respond($res);
+	}
+
+	static function error($error = NULL) {
+		$app = App::get();
+
+		$app->response->setStatus(400);
+
+		return \Katu\Utils\JSON::respond(array(
+			'error' => array(
+				'message' => $error,
+			),
+		));
+	}
+
+	static function getURL($endpoint, $params) {
+		return \Katu\Utils\URL::joinPaths(Config::getApp('apiURL'), $endpoint) . '?' . http_build_query($params);
+	}
+
+	static function useMethod($method, $endpoint, $params = array(), &$curl = NULL) {
+		$curl = new \Curl();
+		$curl->$method(Utils\URL::joinPaths(Config::getApp('apiURL'), $endpoint), $params);
+
+		$response = $curl->response;
+		if (is_array($response)) {
+			$response = Utils\JSON::encode($response);
+		}
+
+		// Success.
+		if ($curl->http_status_code == 200) {
+			return Utils\JSON::decodeAsArray($response);
+		}
+
+		// Error.
+		$array = Utils\JSON::decodeAsArray($response);
+		if (isset($array['error']['message'])) {
+			throw new Exception($array['error']['message']);
+		} else {
+			throw new Exception($curl->error_message, $curl->error_code);
+		}
+
+		return FALSE;
+	}
+
+	static function get($endpoint, $params = array(), &$curl = NULL) {
+		return self::useMethod('get', $endpoint, $params, $curl);
+	}
+
+	static function post($endpoint, $params = array(), &$curl = NULL) {
+		return self::useMethod('post', $endpoint, $params, $curl);
+	}
+
+}
---
+++ src/AmourException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Amour;
+
+class AmourException extends \Exception {
+
+
+
+}
