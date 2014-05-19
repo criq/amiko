e52dcb094a6b55a8dd9ea513e93ae5680e42f17a
Jan Jablunka <jablunka@gmail.com>
2014-05-19 18:16:44 +0200

    incremental

--- src/Amour.php
+++ src/Amour.php
@@ -4,61 +4,37 @@ namespace Amour;
 
 class Amour {
 
-	static function success($res = NULL) {
-		$app = App::get();
-
-		$app->response->setStatus(200);
-
-		return \Katu\Utils\JSON::respond($res);
-	}
-
-	static function error($error = NULL) {
-		$app = App::get();
-
-		$app->response->setStatus(400);
-
-		return \Katu\Utils\JSON::respond(array(
-			'error' => array(
-				'message' => $error,
-			),
-		));
-	}
-
-	static function getURL($endpoint, $params) {
-		return \Katu\Utils\URL::joinPaths(Config::getApp('apiURL'), $endpoint) . '?' . http_build_query($params);
-	}
-
-	static function useMethod($method, $endpoint, $params = array(), &$curl = NULL) {
+	static function useMethod($method, $url, $params = array(), &$curl = NULL) {
 		$curl = new \Curl();
-		$curl->$method(Utils\URL::joinPaths(Config::getApp('apiURL'), $endpoint), $params);
+		$curl->$method(\Katu\Utils\URL::make($url, $params));
 
 		$response = $curl->response;
 		if (is_array($response)) {
-			$response = Utils\JSON::encode($response);
+			$response = \Katu\Utils\JSON::encode($response);
 		}
 
 		// Success.
 		if ($curl->http_status_code == 200) {
-			return Utils\JSON::decodeAsArray($response);
+			return \Katu\Utils\JSON::decodeAsArray($response);
 		}
 
 		// Error.
-		$array = Utils\JSON::decodeAsArray($response);
+		$array = \Katu\Utils\JSON::decodeAsArray($response);
 		if (isset($array['error']['message'])) {
-			throw new Exception($array['error']['message']);
+			throw new AmourException($array['error']['message']);
 		} else {
-			throw new Exception($curl->error_message, $curl->error_code);
+			throw new AmourException($curl->error_message, $curl->error_code);
 		}
 
 		return FALSE;
 	}
 
-	static function get($endpoint, $params = array(), &$curl = NULL) {
-		return self::useMethod('get', $endpoint, $params, $curl);
+	static function get($url, $params = array(), &$curl = NULL) {
+		return static::useMethod('get', $url, $params, $curl);
 	}
 
-	static function post($endpoint, $params = array(), &$curl = NULL) {
-		return self::useMethod('post', $endpoint, $params, $curl);
+	static function post($url, $params = array(), &$curl = NULL) {
+		return static::useMethod('post', $url, $params, $curl);
 	}
 
 }
