7bd7ec4d47a08290b2cd806eb829b93f12c3c571
Jan Jablunka <jablunka@gmail.com>
2015-02-28 09:08:02 +0100

    merge

---
+++ src/Amiko.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Amiko;
+
+class Amiko {
+
+	static function useMethod($method, $url, $params = array(), &$curl = null) {
+		$curl = new \Curl\Curl();
+		$curl->$method(\Katu\Types\TUrl::make($url, $params));
+
+		$response = $curl->response;
+		if (is_array($response)) {
+			$response = \Katu\Utils\JSON::encode($response);
+		}
+
+		// Success.
+		if ($curl->http_status_code == 200) {
+			return \Katu\Utils\JSON::decodeAsArray($response);
+		}
+
+		// Error.
+		$array = \Katu\Utils\JSON::decodeAsArray($response);
+		if (isset($array['error']['message'])) {
+			throw new AmikoException($array['error']['message']);
+		} else {
+			throw new AmikoException($curl->error_message, $curl->error_code);
+		}
+
+		return false;
+	}
+
+	static function get($url, $params = array(), &$curl = null) {
+		return static::useMethod('get', $url, $params, $curl);
+	}
+
+	static function post($url, $params = array(), &$curl = null) {
+		return static::useMethod('post', $url, $params, $curl);
+	}
+
+}
